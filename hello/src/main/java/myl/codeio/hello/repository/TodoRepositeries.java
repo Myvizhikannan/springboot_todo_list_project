d1676f03fa7c9434420a9dc8b5a3e2583950d59c
Myvizhikannan <myvizhikannankanagaraj@gmail.com>
2025-08-28 23:56:03 -0500

    Delete hello directory

--- hello/src/main/java/myl/codeio/hello/repository/TodoRepositeries.java
+++
@@ -1,9 +0,0 @@
-package myl.codeio.hello.repository;
-
-
-import myl.codeio.hello.model.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepositeries extends JpaRepository<ToDo, Long> {
-
-}
